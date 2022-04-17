537f89e243da61f6a3c980726a06bb34b4a49160
Doug Lau <doug.p.lau@gmail.com>
2022-04-17 07:39:48 -0500

    Fixed dot product

--- src/mesh.rs
+++ src/mesh.rs
@@ -71,7 +71,7 @@ impl Vec3 {
         Vec3([x, y, z])
     }
     fn dot(self, rhs: Vec3) -> f32 {
-        self.x() * rhs.x() + self.y() * rhs.y() + self.z() + rhs.z()
+        self.x() * rhs.x() + self.y() * rhs.y() + self.z() * rhs.z()
     }
     #[must_use]
     fn normalize(self) -> Vec3 {
