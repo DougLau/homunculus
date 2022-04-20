3c6fc5abaf27e6b350c0319a089e3dd832784b72
Doug Lau <doug.p.lau@gmail.com>
2022-04-20 16:54:27 -0500

    Use argh to get .muon file name

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,20 @@ pub mod gltf;
 pub mod mesh;
 pub mod solid;
 
+use argh::FromArgs;
+use std::fs::File;
+
+/// Command-line arguments
+#[derive(FromArgs, PartialEq, Debug)]
+struct Args {
+    #[argh(positional)]
+    file: String,
+}
+
 fn main() {
-    let cfg: solid::Config = muon_rs::from_str(SOLID).unwrap();
+    let args: Args = argh::from_env();
+    let file = File::open(&args.file).unwrap();
+    let cfg: solid::Config = muon_rs::from_reader(file).unwrap();
     let mesh = cfg.build();
     gltf::export("test.glb", &mesh).unwrap();
 }
-
-const SOLID: &str = &r#"
-ring:
-  radius: 1.2
-  count: 6
-ring:
-  radius: 1.0
-ring:
-ring:
-  count: 12
-  radius: 0.75
-ring:
-  count: 3
-  radius: 0.2
-ring:
-  count: 1
-  radius: 0.1
-"#;
