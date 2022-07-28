8023eed0d3671a90bfc4fdf6298ea5e52720f0fd
Doug Lau <doug.p.lau@gmail.com>
2022-07-28 16:56:51 -0500

    Improve errors with anyhow

--- hom/src/main.rs
+++ hom/src/main.rs
@@ -2,27 +2,62 @@
 //
 // Copyright (c) 2022  Douglas Lau
 //
+use anyhow::{Context, Result};
 use argh::FromArgs;
 use homunculus::{gltf, solid};
+use std::ffi::OsStr;
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 /// Command-line arguments
 #[derive(FromArgs, PartialEq, Debug)]
 struct Args {
+    /// view model
+    #[argh(switch, short = 'v')]
+    view: bool,
+
+    /// model file name (.hom, .glb, .gltf)
     #[argh(positional)]
-    file: String,
+    model_file: String,
 }
 
 /// Main function
-fn main() {
+fn main() -> Result<()> {
     let args: Args = argh::from_env();
-    let path = Path::new(&args.file);
-    let stem = path.file_stem().unwrap();
-    let out = path.with_file_name(Path::new(stem).with_extension("glb"));
-    let writer = File::create(out).unwrap();
-    let file = File::open(path).unwrap();
-    let cfg: solid::Config = muon_rs::from_reader(file).unwrap();
+    let _path = args.build_model()?;
+    if args.view {
+        // TODO: view model
+    }
+    Ok(())
+}
+
+impl Args {
+    /// Build glTF model
+    fn build_model(&self) -> Result<PathBuf> {
+        let path = Path::new(&self.model_file);
+        let stem = path.file_stem().context("Invalid model_file name")?;
+        match path.extension() {
+            Some(ext) if ext == "glb" || ext == "gltf" => {
+                if !self.view {
+                    anyhow::bail!("{} already glTF model", path.display());
+                }
+                Ok(path.into())
+            }
+            _ => build_homunculus(&path, &stem),
+        }
+    }
+}
+
+/// Build homunculus model
+fn build_homunculus(path: &Path, stem: &OsStr) -> Result<PathBuf> {
+    let file = File::open(path)
+        .with_context(|| format!("{} not found", path.display()))?;
+    let cfg: solid::Config =
+        muon_rs::from_reader(file).context("Invalid homunculus model")?;
     let mesh = cfg.build();
-    gltf::export(&writer, &mesh).unwrap();
+    let out = path.with_file_name(Path::new(stem).with_extension("glb"));
+    let writer = File::create(&out)
+        .with_context(|| format!("Cannot create {}", out.display()))?;
+    gltf::export(&writer, &mesh).context("Writing glTF")?;
+    Ok(out)
 }
