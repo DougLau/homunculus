953f597ace08b69478704f9b842bdf150a7db8c8
Doug Lau <doug.p.lau@gmail.com>
2023-11-17 19:12:00 -0600

    Updated example program

--- homunculus/examples/pyramid.rs
+++ homunculus/examples/pyramid.rs
@@ -1,12 +1,19 @@
 // pyramid example
-use homunculus::{Husk, Ring};
+use anyhow::Result;
+use homunculus::{Husk, Ring, Shading};
 use std::fs::File;
 
-fn main() {
+fn main() -> Result<()> {
     let mut husk = Husk::new();
-    let base = Ring::default().spoke(1.0).spoke(1.0).spoke(1.0).spoke(1.0);
-    husk.ring(base).unwrap();
-    husk.ring(Ring::default().spoke(0.0)).unwrap();
-    let file = File::create("pyramid.glb").unwrap();
-    husk.write_gltf(file).unwrap();
+    let base = Ring::default()
+        .shading(Shading::Flat)
+        .spoke(1.0)
+        .spoke(1.0)
+        .spoke(1.0)
+        .spoke(1.0);
+    husk.ring(base)?;
+    husk.ring(Ring::default().spoke(0.0))?;
+    let file = File::create("pyramid.glb")?;
+    husk.write_gltf(file)?;
+    Ok(())
 }
